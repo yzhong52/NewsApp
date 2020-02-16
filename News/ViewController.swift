714753b2e83489b39683342c6e87838bbec45bf5
Yuchen Zhong <yzhong.cs@gmail.com>
2020-02-16 10:17:31 -0500

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Yuchen Zhong on 2020-02-16.
+//  Copyright © 2020 Yuchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
