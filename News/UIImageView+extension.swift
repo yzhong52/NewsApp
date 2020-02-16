caeb618d51b0d2032a9d84d8758a54afbbdd9292
Yuchen Zhong <yzhong.cs@gmail.com>
2020-02-16 17:06:15 -0500

    Oops, commented out the UIImageView extension by mistake. Adding it back here.

--- News/UIImageView+extension.swift
+++ News/UIImageView+extension.swift
@@ -1,22 +1,22 @@
-////
-////  UIImageView+extension.swift
-////  News
-////
-////  Created by Yuchen Zhong on 2020-02-16.
-////  Copyright © 2020 Yuchen. All rights reserved.
-////
 //
-//import UIKit
-//import Alamofire
+//  UIImageView+extension.swift
+//  News
 //
-//extension UIImageView {
-//    func load(url: URLConvertible) {
-//        backgroundColor = .lightGray
-//        image = nil
+//  Created by Yuchen Zhong on 2020-02-16.
+//  Copyright © 2020 Yuchen. All rights reserved.
 //
-//        ImageManager.shared.load(url: url) { [weak self] image in
-//            self?.backgroundColor = nil
-//            self?.image = image
-//        }
-//    }
-//}
+
+import UIKit
+import Alamofire
+
+extension UIImageView {
+    func load(url: URLConvertible) {
+        backgroundColor = .lightGray
+        image = nil
+
+        ImageManager.shared.load(url: url) { [weak self] image in
+            self?.backgroundColor = nil
+            self?.image = image
+        }
+    }
+}
